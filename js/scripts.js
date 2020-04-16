5091ef7b073409ca4a14622608bc94a1d3e290b5
Tamara Sirakanyan <tamarasirakanyan@tamsmac.local>
2020-04-16 22:59:31 +0400

    added math operations

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,22 @@ console.log('val', displayNum.value)
 
 function display(n) {
     let d = displayNum.value =displayNum.value + n
+    console.log('numbers: ', d, 'num legth: ', d.length, 'lastChar: ', d[d.length - 2])
+    console.log('what is',typeof(parseFloat(d)))
     //clear the number display
     if (n === 'c') {
+        const clearBtn = document.getElementById('#clear');
         console.log('clear')
         displayNum.value = ''
     }
     //add decimal pointer
     else if (n === '.'){
+        if (d.length == 1){
+            displayNum.value = '0.5'
+        } if(d[d.length - 2] === '+' ) {
+            console.log('here')
+        }
+        const clearBtn = document.getElementById('#clear');
         d = d+'.'
     }
 }
