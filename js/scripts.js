c69f826ba7669c33efba3c592669fdb64e68fa3c
Tamara Sirakanyan <tamarasirakanyan@tamsmac.local>
2020-04-16 21:50:29 +0400

    added math functional part

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 let displayNum =  document.getElementById('num-display')
+console.log('val', displayNum.value)
 
 function display(n) {
     let d = displayNum.value =displayNum.value + n
-    console.log(d)
+    //clear the number display
+    if (n === 'c') {
+        console.log('clear')
+        displayNum.value = ''
+    }
+    //add decimal pointer
+    else if (n === '.'){
+        d = d+'.'
+    }
+}
+
+function equal() {
+    let d = displayNum.value
+    if(d) {
+        displayNum.value = eval(d)
+    }
+}
+
+function clear(){
+    console.log('here')
+    displayNum.value = '';
 }
