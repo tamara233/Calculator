69ea5ac23fb7f434e943dd4b5ce8c07fd49219c8
Tamara Sirakanyan <tamarasirakanyan@tamsmac.local>
2020-04-21 17:55:52 +0400

    fixed math operators

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,53 @@
-let displayNum =  document.getElementById('num-display')
-console.log('val', displayNum.value)
+const displayNum =  document.getElementById('num-display')
+const operators = [ '+', '-', '*', '/' ];
 
 function display(n) {
-    let d = displayNum.value =displayNum.value + n
-    console.log('numbers: ', d, 'num legth: ', d.length, 'lastChar: ', d[d.length - 2])
-    console.log('what is',typeof(parseFloat(d)))
+    let d = displayNum.value = displayNum.value + n
+    console.log('val', d, displayNum.value)
+
     //clear the number display
     if (n === 'c') {
         const clearBtn = document.getElementById('#clear');
-        console.log('clear')
         displayNum.value = ''
+        d.length = 0;
     }
+
+    //toggle operators disabled to enabled   
+    let list = document.getElementsByClassName('ops');
+    for (let item of list) {
+        if(displayNum.value.length < 1 || displayNum.value == 'c'){
+            item.disabled = true;
+        } else {
+            item.disabled = false;
+        }
+        //prevent outputting multiple operators
+        if (operators.includes(d[d.length - 1])){
+            //if (item.value == n) {
+             item.disabled = true;
+            //}
+            //  else {
+            //     let minusLast = displayNum.value.substring(0, displayNum.value.length - 1)
+            //     displayNum.value = minusLast.substring(0, displayNum.value.length - 1) + n
+            //     console.log('from else',displayNum.value, 'minus last: ', displayNum.value.substring(0, displayNum.value.length - 1), 'current: ', n)
+            // }
+        }    
+    }
+    
     //add decimal pointer
-    else if (n === '.'){
+     if (n === '.'){
         if (d.length == 1){
             displayNum.value = '0.5'
-        } if(d[d.length - 2] === '+' ) {
-            console.log('here')
+        }     
+        if(operators.includes(d[d.length - 2])) {
+            displayNum.value = displayNum.value.substring(0, displayNum.value.length -1) + '0.5'            
         }
-        const clearBtn = document.getElementById('#clear');
-        d = d+'.'
     }
 }
 
 function equal() {
-    let d = displayNum.value
+    const d = displayNum.value
     if(d) {
         displayNum.value = eval(d)
     }
 }
 
-function clear(){
-    console.log('here')
-    displayNum.value = '';
-}
